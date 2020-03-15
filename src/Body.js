85daf5b509be09d540ca265249543781fcdb6183
hamohuh <hamodroid@gmail.com>
2020-03-15 15:05:14 +0200

    Added some simple styling

--- src/Body.js
+++ src/Body.js
@@ -3,11 +3,20 @@ import React from 'react'
 
 function Body(props) {
     const { images } = props;
-    return (
-        <div className='images'>
-            {images.map((img, i) => <img className='gif-img' key={i} src={img.images.original.url}></img>)}
-        </div>
-    )
+
+    if (images.length === 0) {
+        return (<div className='images'>
+            <p className='no-results'>There are no results to show, please start your search.</p>
+        </div>)
+    } else {
+        return (
+            <div className='images'>
+                {images.map((img, i) => <img className='gif-img' key={i} src={img.images.original.url}></img>)}
+            </div>
+        )
+    }
+
+
 }
 
 export default Body;
